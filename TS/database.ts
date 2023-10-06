89f4e8abea53ed63b8a6efd9640a74b516e50ad7
Victor Schandel <schandel2u@etu.univ-lorraine.fr>
2023-10-06 16:01:18 +0200

    HTML ET CSS, CSS a débug

---
+++ TS/database.ts
@@ -0,0 +1,6 @@
+export const dbConfig = {
+    user: 'ponzo1u_appli',
+    password: '72622503',
+    host: 'devbdd.iutmetz.univ-lorraine.fr', 
+    database: 'ponzo1u_sae501',
+};
---
+++ TS/loginRegister.js
@@ -0,0 +1,16 @@
+var loginForm = document.getElementById('loginFormulaire');
+var registerForm = document.getElementById('registerFormulaire');
+
+loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("jirejguirjgiotfjgbiotfjg");
+
+    
+
+});
+
+
+registerForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("TTTTTTTTEST");
+});

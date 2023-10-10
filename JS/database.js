964a40a1bc5000f4b9bfdba00ba61cc4f745dca9
Yoni Rigaut <yoni.rigaut6@etu.univ-lorraine.fr>
2023-10-10 10:04:29 +0200

    tentative de connection

--- JS/database.js
+++ JS/database.js
@@ -1,8 +1,29 @@
 "use strict";
 
-export const dbConfig = {
+const express = require("express");
+const mysql = require("mysql");
+const port = process.env.PORT;
+
+const app = express();
+
+export const dbConfig =  mysql.createConnection({
+    host: 'devbdd.iutmetz.univ-lorraine.fr',
     user: 'ponzo1u_appli',
     password: '72622503',
-    host: 'devbdd.iutmetz.univ-lorraine.fr', 
     database: 'ponzo1u_sae501',
-};
+    // socketPath: "/tmp/mysql/mysql.sock"
+});
+
+dbConfig.connect(err => {
+    if(err) {
+        console.error("Erreur : "+err.stack);
+        return;
+    }
+    console.log("Connection réussie");
+});
+
+dbConfig.query("query")
+
+app.listen(port, () => {
+    console.log("Serveur en ligne");
+});

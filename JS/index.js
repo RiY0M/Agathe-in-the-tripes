50851f167a0304aa76bbe531cec5526e3967d554
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-10-10 13:58:59 +0200

    liaison formulaire -> pseudo invité + commentaires index.js

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,66 @@
+// VARIABLES TEST
 let isConnected = false;
 let pseudo = "LLazry";
 let alreadyAGame = true;
 
+
+// FORMULAIRE CONNEXION / INSCRIPTION
 let loginRegisterForm = document.querySelector(".loginRegisterPopUp");
+// par défaut, on cache ce formulaire
 loginRegisterForm.style.visibility = "hidden";
 
 
+// BOUTONS PRINCIPAUX
 let loginRegisterButton = document.querySelector("#login-register-button");
 let nouvellePartyButton = document.querySelector("#restart-game-button");
 let continuePartyButton = document.querySelector("#start-game-button");
 
+// PSEUDO DU JOUEUR
 let headerPseudo = document.querySelector("#header-player-name");
 
 
+// si le joueur n'a pas de partie en cours on cache le bouton "nouvelle partie"
 if (!alreadyAGame) { 
     continuePartyButton.style.display = "none";
     nouvellePartyButton.style.marginTop = "-10rem";
 }
 
 
+// si le joueur est déjà connecté :
 if (isConnected) {
+    // on affiche son pseudo
     headerPseudo.textContent = pseudo;
+
+    // on change le bouton pour "déconnexion"
     loginRegisterButton.value = "Déconnexion";
 }
 
-loginRegisterButton.addEventListener("click", () => {
+// si le joueur n'est pas connecté :
+else {
+    // on affiche le nom "Invité"
+    headerPseudo.textContent = pseudo;
 
-    let divInBody = document.querySelectorAll("body>div");
-    divInBody.forEach((div) => div.style.visibility = "hidden");
+    // on permet à l'utilisateur de voir le formulaire d'inscription et de connexion
 
-    loginRegisterForm.style.visibility = "visible";
-})
+    // DEPUIS LE BOUTON CONNEXION / INSCRIPTION
+    loginRegisterButton.addEventListener("click", () => {
+
+        // on masque tout ce qui se trouve dans le body pour ne laisser que le formulaire
+        let divInBody = document.querySelectorAll("body>div");
+        divInBody.forEach((div) => div.style.visibility = "hidden");
+        
+        // affichage du formulaire connexion / inscription
+        loginRegisterForm.style.visibility = "visible";
+    })
+
+    // DEPUIS LE PSEUDO DU JOUEUR
+    headerPseudo.addEventListener("click", () => {
+
+        // on masque tout ce qui se trouve dans le body pour ne laisser que le formulaire
+        let divInBody = document.querySelectorAll("body>div");
+        divInBody.forEach((div) => div.style.visibility = "hidden");
+        
+        // affichage du formulaire connexion / inscription
+        loginRegisterForm.style.visibility = "visible";
+    })
+}

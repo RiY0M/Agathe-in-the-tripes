7eb64555b1b9f4df22ee913fc74dc84c51ba27ea
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-10-25 09:50:23 +0200

    creation fonctionnalités de base lvl-1

---
+++ JS/phaser/lvl-1/lvl1.js
@@ -0,0 +1,102 @@
+// configuration de la taille de l'écran, du type de jeu et des fonctions par défaut
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#94251e",
+    physics: { default: 'arcade' },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+//$ CREATION FENETRE PHASER $//
+let game = new Phaser.Game(config);
+
+
+function preload()
+{
+    // chargement de tous les sprites
+    loadImages(this);
+}
+
+function create()
+{
+    //~ SPRITE AGATHE ~//
+    createAgathe(this);
+
+    
+    //! DETECTION DU CLAVIER !//
+    cursors = this.input.keyboard.createCursorKeys();
+
+
+    //^ ANIMATIONS AGATHE (SPRITES) ^//
+
+    //? MOVING ANIMS ?//
+    createAnims(this);
+    
+    //? AFK ANIMS ?//
+    createAFK(this);    
+}
+
+function update()
+{
+    //^ ANIMATIONS AGATHE (CLAVIER) ^//
+
+    /* GAUCHE */
+    if (cursors.left.isDown && cursors.right.isUp && cursors.up.isUp && cursors.down.isUp)
+    {
+        // vitesse et direction du déplacement
+        agathe.setVelocityX(-160);
+        // animation sprite
+        agathe.anims.play("left", true);
+        // last frame facing afk
+        lastFrame = 4;
+    }
+
+    /* DROITE */
+    else if (cursors.right.isDown && cursors.left.isUp && cursors.up.isUp && cursors.down.isUp)
+    {
+        // vitesse et direction du déplacement
+        agathe.setVelocityX(160);
+        // animation sprite
+        agathe.anims.play("right", true);
+        // last frame facing afk
+        lastFrame = 8;
+    }
+
+    /* HAUT */
+    else if (cursors.up.isDown && cursors.down.isUp && cursors.left.isUp && cursors.right.isUp)
+    {
+        // vitesse et direction du déplacement
+        agathe.setVelocityY(-160);
+        // animation sprite
+        agathe.anims.play("up", true);
+        // last frame facing afk
+        lastFrame = 12;
+    }
+
+    /* BAS */
+    else if (cursors.down.isDown && cursors.up.isUp && cursors.left.isUp && cursors.right.isUp)
+    {
+        // vitesse et direction du déplacement
+        agathe.setVelocityY(160);
+        // animation sprite
+        agathe.anims.play("down", true);
+        // last frame facing afk
+        lastFrame = 0;
+    }
+
+    /* AFK */
+    else
+    {
+        // si aucune touche du clavier n'est appuyée : on arrête agathe
+        agathe.setVelocityX(0);
+        agathe.setVelocityY(0);
+        // pose du joueur selon la dernière touche (gauche/droite/haut/bas)
+        agathe.anims.play("afk-" + lastFrame);
+    }
+}
---
+++ JS/phaser/lvl-1/variables.js
@@ -0,0 +1,3 @@
+let agathe;                 // agathe personnage
+let cursors;                // détection clavier
+let lastFrame = 12;         // last frame facing afk

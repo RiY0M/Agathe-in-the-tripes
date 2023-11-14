9a10c33bd11b6b4fb395acd25630c8ce282b8474
Yoni Rigaut <yoni.rigaut6@etu.univ-lorraine.fr>
2023-11-14 14:03:23 +0100

    ajout kayou

--- JS/phaser/lvl-2/createSprites.js
+++ JS/phaser/lvl-2/createSprites.js
@@ -102,50 +102,35 @@ function createRocks(scene)
     // groupement de cailloux
     rocks = scene.physics.add.staticGroup();
 
-    // mix x 500
+    // Xmin 500
     // grossses pierres
-    // rocks.push(scene.physics.add.image(50, 150, "rock-3"));
-    // rocks.push(scene.physics.add.image(75, 350, "rock-3"));
-    // rocks.setGravityY(0);
-    rocks.create(900, 300, "rock-3");
-    rocks.create(850, 350, "rock-3");
-    // rocks.create(270, 500, "rock-3");
-    // rocks.create(320, 425, "rock-3");
-    // rocks.create(270, 125, "rock-3");
-    // rocks.create(430, 290, "rock-3");
-    // rocks.create(465, 215, "rock-3");
-    // rocks.create(600, 455, "rock-3");
-    // rocks.create(740, 400, "rock-3");
-    // rocks.create(695, 335, "rock-3");
-    // rocks.create(650, 145, "rock-3");
 
-    // // double-pierre
-    // rocks.create(65, 190, "rock-2");
-    // rocks.create(115, 370, "rock-2");
-    // rocks.create(670, 300, "rock-2");
-    // rocks.create(690, 150, "rock-2");
+    const rocks0 = [
+        [500, 230],
+        [500, 360],
+        [513, 260],
+    ];
 
-    // // cailloux pointu neige
-    // rocks.create(230, 110, "rock-0");
-    // rocks.create(115, 440, "rock-0");
-    // rocks.create(430, 195, "rock-0");
-    // rocks.create(285, 235, "rock-0");
-    // rocks.create(310, 255, "rock-0");
-    // rocks.create(315, 500, "rock-0");
-    // rocks.create(785, 95, "rock-0");
-    // rocks.create(550, 425, "rock-0");
+    rocks0.forEach(rock0 => rocks.create(rock0[0], rock0[1], "rock-0"));
 
-    // // petit cailloux
-    // rocks.create(115, 345, "rock-1");
-    // rocks.create(285, 275, "rock-1");
-    // rocks.create(230, 135, "rock-1");
-    // rocks.create(160, 310, "rock-1");
-    // rocks.create(400, 415, "rock-1");
-    // rocks.create(565, 445, "rock-1");
-    // rocks.create(535, 460, "rock-1");
-    // rocks.create(775, 115, "rock-1");
-    // rocks.create(660, 325, "rock-1");
-    // rocks.create(350, 445, "rock-1");
+    const rocks1 = [
+        [540, 270],
+    ];
+
+    rocks1.forEach(rock1 => rocks.create(rock1[0], rock1[1], "rock-1"));
+
+    const rocks2 = [
+        [540, 270],
+    ];
+
+    rocks2.forEach(rock2 => rocks.create(rock2[0], rock2[1], "rock-2"));
+
+    const rocks3 = [
+        [850, 350],
+        [900, 300],
+    ];
+
+    rocks3.forEach(rock3 => rocks.create(rock3[0], rock3[1], "rock-3"));
 }
 
 

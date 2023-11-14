79b0401b0eb4c7a21e593cc1cad2bc0286f0713d
V.Schandel <V.Schandel>
2023-11-14 09:30:23 +0100

    Derniere tentative TileMap

--- JS/phaser/lvl-4/createSprites.js
+++ JS/phaser/lvl-4/createSprites.js
@@ -10,22 +10,5 @@ function createAgathe(scene)
 
 
 function setupCollisions(scene) {
-    const map = scene.make.tilemap({ key: 'map', tileWidth: 16, tileHeight: 16 });
-
-    // Créez des couches pour le sol et les murs
-    const solLayer = map.getLayer('Sol');
-    const mursLayer = map.getLayer('Murs');
-
-    // Activer les collisions pour la couche des murs en utilisant l'intervalle correct
-    map.setCollisionBetween(1, 337, true, 'Murs');
-
-    // Configurer les collisions avec le joueur (agathe)
-    scene.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     
-    // Activer les collisions avec la couche du sol (pour éviter les passages à travers le sol)
-    scene.physics.add.collider(agathe, solLayer);
-
-    // Désactiver la collision entre le sol et les murs (pour permettre à l'utilisateur de se déplacer librement sur le sol)
-    map.setCollisionBetween(1, 337, false, 'Sol');
 }
-
--- JS/phaser/lvl-4/loadSprites.js
+++ JS/phaser/lvl-4/loadSprites.js
@@ -2,5 +2,5 @@ function loadImages(scene)
 {
     // chargement sprites agathe
     scene.load.spritesheet("agathe", "../../img/assets/agathe_sprite.png", { frameWidth: 32, frameHeight: 48 });
-    scene.load.tilemapTiledJSON("map", "../../img/TileMaps/mapLv4.json");
+    //scene.load.tilemapTiledJSON("map", "../../img/TileMaps/map.json");
 }

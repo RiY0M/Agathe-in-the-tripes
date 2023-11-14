d8b8b40ff117882a178f6d36d8ee8dd870a483d6
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-11-14 13:50:36 +0100

    commentaire affichage coeurs

--- JS/phaser/common/displayHearts.js
+++ JS/phaser/common/displayHearts.js
@@ -1,20 +1,27 @@
+// nombre de coeurs par défaut (3)
 let nbHearts = 3;
+// immortalité par défaut (non)
 let isInvicible = false;
 
 
+// div affichage des coeurs
 let lifeDiv = document.createElement("div");
 lifeDiv.className += "life-div";
 
+// image des coeurs
 let heartImage = new Image();
 heartImage.className += "heart-image";
 
+// lien de l'image des coeurs en fonction du nb de coeurs
 heartImage.src = "../../../img/assets/" + nbHearts + "-heart.png";
 lifeDiv.appendChild(heartImage);
 
+// affichage de la vie
 document.querySelector("body").appendChild(lifeDiv);
 
 
 
+// rafraichissement des coeurs
 function reloadNbHearts()
 {
     document.querySelector(".heart-image").src = "../../../img/assets/" + nbHearts + "-heart.png";

f1e290e3e713cc6b20789ca3aea3b3eadd13ca61
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-11-23 13:42:02 +0100

    configuration vierge lvl-5

---
+++ JS/phaser/lvl-5/loadSprites.js
@@ -0,0 +1,5 @@
+function loadImages(scene)
+{
+    // chargement sprites agathe
+    scene.load.spritesheet("agathe", "../../../img/assets/agathe_sprite.png", { frameWidth: 32, frameHeight: 48 });
+}
---
+++ JS/phaser/lvl-5/variables.js
@@ -0,0 +1,9 @@
+let agathe;                     // agathe personnage
+let cursors;                    // détection clavier
+let lastFrame = 12;             // last frame facing afk
+
+
+function loadSpriteVariables(scene)
+{
+    
+}

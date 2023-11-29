addbeef287d5354020dbe7bc951f4ae79305b240
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-11-29 09:52:02 +0100

    premier dialogue avec le vieux

--- JS/phaser/lvl-5/functions.js
+++ JS/phaser/lvl-5/functions.js
@@ -7,8 +7,13 @@ function dialogueWithOld(scene, agathe)
         // on regarde si on a déjà parlé au vieux pour lancer le premier dialogue
         if (dialogueOldPoop == 0)
         {
-            // console.log("should display area");
+            // changement de l'image et du texte
+            changeImgTextDialogue("../../../img/assets/old-on-poop.png", oldDialogueList[0]);
+
+            // affichage de la zone de dialogue
             displayTextArea(dialogueArea);
+
+            // on passe au dialogue suivant
             dialogueOldPoop++;
         }
     }
@@ -18,5 +23,38 @@ function dialogueWithOld(scene, agathe)
 //$ AFFICHAGE ZONE TEXTE $//
 function displayTextArea()
 {
+    // on indique que la zone est affichée
+    isDialogueAreaDisplayed = true;
+    // affichage zone de dialogue
     dialogueArea.style.visibility = "visible";
 }
+
+
+//$ AFFICHAGE DIALOGUE ET IMAGE $//
+function changeImgTextDialogue(imagePath, text)
+{
+    // changement image
+    document.querySelector("#dialogue-area-pp").src = imagePath;
+
+    // changement texte
+    document.querySelector("#dialogue-text").innerHTML = text;
+}
+
+
+//$ CHECK SI CHANGEMENT DIALOGUE $//
+document.addEventListener('keypress', (event) => {
+    // on regarde si on a appuyé sur la touche entrée et si un dialogue est lancé
+    if (event.key === 'Enter' && isDialogueAreaDisplayed === true) {
+
+        // si on arrive au dernier dialogue on ferme la zone d'affichage
+        if (dialogueOldPoop === oldDialogueList.length) {
+
+            dialogueArea.style.visibility = "hidden";
+            isDialogueAreaDisplayed = false;
+        }
+
+        // on passe au dialogue suivant
+        changeImgTextDialogue("../../../img/assets/old-on-poop.png", oldDialogueList[dialogueOldPoop]);
+        dialogueOldPoop++;
+    }
+});
--- JS/phaser/lvl-5/variables.js
+++ JS/phaser/lvl-5/variables.js
@@ -1,11 +1,19 @@
-let agathe;                    // agathe personnage
-let cursors;                   // détection clavier
-let lastFrame = 8;             // last frame facing afk
-let canvasBorder;              // bordure du canvas
-let oldOnPoop;                 // image pnj sur caca
-let flatThings;                // images plates (sang, caca, ...)
-let dialogueOldPoop = 0;       // check si on a déjà parlé au vieux
-let dialogueArea;              // zone de dialogue
+let agathe;                             // agathe personnage
+let cursors;                            // détection clavier
+let lastFrame = 8;                      // last frame facing afk
+let canvasBorder;                       // bordure du canvas
+let oldOnPoop;                          // image pnj sur caca
+let flatThings;                         // images plates (sang, caca, ...)
+let dialogueOldPoop = 0;                // check si on a déjà parlé au vieux
+let dialogueArea;                       // zone de dialogue
+let isDialogueAreaDisplayed = false;    // on vérifie si la zone est affichée ou non
+
+let oldDialogueList = [                 // dialogues du vieux au caca
+    "Salut la jeunesse !<br>Alors comme ça toi aussi tu es coincée dans ce monstre gamine ?",
+    "La sortie ? Ça fait logntemps que j'ai abandonné l'idée de l'atteindre...\nElle semble bloquée par cet amas de ... je préfère pas trop savoir en fait.",
+    "Si seulement j'avais un moyen de tout faire sauter... Un bon coup de dynamite règlerai notre problème !",
+    "T'en penses quoi gamine ?"
+]
 
 
 function loadSpriteVariables(scene)

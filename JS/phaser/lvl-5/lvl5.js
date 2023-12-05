e6e33e4ae7c5f418be4d7d24bd46e861d6c4185b
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-12-05 09:56:57 +0100

    on empêche de se déplacer pdt qu'on parle à un pnj

--- JS/phaser/lvl-5/lvl5.js
+++ JS/phaser/lvl-5/lvl5.js
@@ -78,50 +78,63 @@ function update()
 
 
     /* GAUCHE */
-    if (cursors.left.isDown)
-    {
-        // vitesse et direction du déplacement
-        agathe.setVelocityX(-160);
-        // animation sprite
-        agathe.anims.play("left", true);
-        // last frame facing afk
-        lastFrame = 4;
-    }
 
-    /* DROITE */
-    else if (cursors.right.isDown)
+    // on empêche de se déplacer si on est au milieu d'un dialogue
+    if (!isDialogueAreaDisplayed)
     {
-        // vitesse et direction du déplacement
-        agathe.setVelocityX(160);
-        // animation sprite
-        agathe.anims.play("right", true);
-        // last frame facing afk
-        lastFrame = 8;
-    }
+        if (cursors.left.isDown)
+        {
+            // vitesse et direction du déplacement
+            agathe.setVelocityX(-160);
+            // animation sprite
+            agathe.anims.play("left", true);
+            // last frame facing afk
+            lastFrame = 4;
+        }
 
-    /* HAUT */
-    else if (cursors.up.isDown)
-    {
-        // vitesse et direction du déplacement
-        agathe.setVelocityY(-160);
-        // animation sprite
-        agathe.anims.play("up", true);
-        // last frame facing afk
-        lastFrame = 12;
-    }
+        /* DROITE */
+        else if (cursors.right.isDown)
+        {
+            // vitesse et direction du déplacement
+            agathe.setVelocityX(160);
+            // animation sprite
+            agathe.anims.play("right", true);
+            // last frame facing afk
+            lastFrame = 8;
+        }
 
-    /* BAS */
-    else if (cursors.down.isDown)
-    {
-        // vitesse et direction du déplacement
-        agathe.setVelocityY(160);
-        // animation sprite
-        agathe.anims.play("down", true);
-        // last frame facing afk
-        lastFrame = 0;
-    }
+        /* HAUT */
+        else if (cursors.up.isDown)
+        {
+            // vitesse et direction du déplacement
+            agathe.setVelocityY(-160);
+            // animation sprite
+            agathe.anims.play("up", true);
+            // last frame facing afk
+            lastFrame = 12;
+        }
 
-    /* AFK */
+        /* BAS */
+        else if (cursors.down.isDown)
+        {
+            // vitesse et direction du déplacement
+            agathe.setVelocityY(160);
+            // animation sprite
+            agathe.anims.play("down", true);
+            // last frame facing afk
+            lastFrame = 0;
+        }
+
+        /* AFK */
+        else
+        {
+            // si aucune touche du clavier n'est appuyée : on arrête agathe
+            agathe.setVelocityX(0);
+            agathe.setVelocityY(0);
+            // pose du joueur selon la dernière touche (gauche/droite/haut/bas)
+            agathe.anims.play("afk-" + lastFrame);
+        }
+    }
     else
     {
         // si aucune touche du clavier n'est appuyée : on arrête agathe

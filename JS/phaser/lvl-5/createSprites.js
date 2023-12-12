15d69bb67178d2780bcc1e1527505cac7b1a60ba
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2023-12-12 11:23:00 +0100

    changement z-index agathe pour passer devant poudre

--- JS/phaser/lvl-5/createSprites.js
+++ JS/phaser/lvl-5/createSprites.js
@@ -2,7 +2,7 @@
 function createAgathe(scene)
 {
     // ajout d'agathe à la fenêtre
-    agathe = scene.physics.add.sprite(15, 300, "agathe")
+    agathe = scene.physics.add.sprite(15, 300, "agathe").setDepth(10)
         .setSize(21, 8)
         .setOffset(5, 40);
     // on définit les collisions avec la bordure

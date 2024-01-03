f18b5f457098e0673cb448f59e73cab1627db8e0
PONZO Justin <justin.ponzo9@etu.univ-lorraine.fr>
2024-01-03 14:30:51 +0100

    passage liste chainée à liste doublement chainée

--- JS/phaser/lvl-5/chainedListRatPath.js
+++ JS/phaser/lvl-5/chainedListRatPath.js
@@ -7,10 +7,11 @@ class Maillon
     _velX;
     _velY;
     _anim;
+    _prev;
     _next;
 
     // CONSTRUCTEUR  
-    constructor(id = 0, x = 0, y = 0, velX = 0, velY = 0, anim = "", next = null) {
+    constructor(id = 0, x = 0, y = 0, velX = 0, velY = 0, anim = "", prev = null, next = null) {
         
         this._id = id;
         this._x = x;
@@ -18,6 +19,7 @@ class Maillon
         this._velX = velX;
         this._velY = velY;
         this._anim = anim;
+        this._prev = prev;
         this._next = next;
     }
 
@@ -44,6 +46,10 @@ class Maillon
 
     get anim() {
         return this._anim;
+    }
+
+    get prev() {
+        return this._prev;
     }
 
     get next() {
@@ -75,8 +81,34 @@ class Maillon
         this._anim = val;
     }
 
+    set prev(val) {
+        this._prev = val;
+    }
+
     set next(val) {
         this._next = val;
+    }
+
+    // METHODES
+    switchAnim()
+    {
+        switch (this._anim) {
+
+            case "left-rat":
+                return "right-rat";
+
+            case "right-rat":
+                return "left-rat";
+
+            case "up-rat":
+                return "down-rat";
+
+            case "down-rat":
+                return "up-rat"
+        
+            default:
+                break;
+        }
     }
 }
   
@@ -106,28 +138,56 @@ class ListeChainee
     }
     
     // METHODES
+    estVide()
+    {
+        // La liste est vide si la tête vaut null
+        return this._head == null;
+    }
+
     ajouter(id, x, y, velX, velY, anim)
     {
-        // création d'un nouveau maillon
-        let nouveauMaillon = new Maillon(id, x, y, velX, velY, anim, this._head);
-        // la tête est le nouveau maillon
-        this._head = nouveauMaillon;
+        // liste vide : tete et fin correspondent au nouveau maillon
+        if (this.estVide())
+        {
+            let nouveauMaillon = new Maillon(id, x, y, velX, velY, anim);
+            this._head = nouveauMaillon;
+            this._tail = this._head;
+        }
 
-        // si le maillon après la tête est nul, la tête devient la queue
-        if (this._head.next == null) this._tail = this._head;
+        // liste non-vide : nouveau maillon se place en tete
+        else
+        {
+            // création d'un nouveau maillon
+            let nouveauMaillon = new Maillon(id, x, y, velX, velY, anim, null, this._head);
+            // la tête est le nouveau maillon
+            this._head.prev = nouveauMaillon;
+            this._head = nouveauMaillon;
+        }
     }
 
     ajouterFin(nouveauMaillon)
     {
-        // la queue devient le nouveau maillon
-        this._tail.next = nouveauMaillon;
-        this._tail = nouveauMaillon;
+        // liste vide : tete et fin correspondent au nouveau maillon
+        if (this.estVide())
+        {
+            this._tail = nouveauMaillon;
+            this._head = this._tail;
+        }
 
-        // si la tête est vide, la tête est la queue
-        if (this._head === null) this._head = this._tail;
+        // liste non-vide : nouveau maillon se place en tete
+        else
+        {
+            //attribution d'un prev et next
+            nouveauMaillon.prev = this._tail;
+            nouveauMaillon.next = null;
+
+            // la queue est le nouveau maillon
+            this._tail.next = nouveauMaillon;
+            this._tail = nouveauMaillon;
+        }
     }
   
-    affichage()
+    affichageLog()
     {
         // le premier maillon est la tête
         let current = this._head;
@@ -141,17 +201,73 @@ class ListeChainee
         }
     }
 
+    affichageGraph()
+    {
+        // graphique
+        let graph = "";
+        let row = "";
+
+        // le premier maillon est la tête
+        let current = this._head;
+
+        while (current != null) {
+
+            if (current.id < 10) row += "+---+     "
+            else row += "+----+     ";
+            
+            // affichage de l'id du maillon
+            graph += "| " + current.id + " | <-> ";
+
+            // on regarde le maillon d'après
+            current = current.next;
+        }
+
+        console.log(' ' + row, '\n', graph.slice(0, -4), '\n', row);
+    }
+
     decalage()
     {
         // on stock les données de la tête dans une variable temporaire
-        let currentHead = new Maillon(this._head.id, this._head.x, this._head.y, this._head.velX, this._head.velY, this._head.anim, null);
+        let currentHead = new Maillon(this._head.id, this._head.x, this._head.y, this._head.velX, this._head.velY, this._head.anim, this._tail, null);
 
         // on déplace la tête vers la 2e valeur
         this._head = this._head.next;
+        this._head.prev = null;
 
         // on ajoute notre ancienne tête en fin de queue
         this.ajouterFin(currentHead);
     }
+
+    // inversedDecalage()
+    // {
+    //     // on stock les données de la queue dans une variable temporaire
+    //     let currentTail = new Maillon(this._tail.id, this._tail.x, this._tail.y, this._tail.velX, this._tail.velY, this._tail.anim, null);
+
+    //     // on déplace la queue vers la 2e valeur
+    //     this._head = this._tail.next;
+
+    //     // on ajoute notre ancienne queue en tête
+    //     this.ajouter(currentTail);
+    // }
+
+    // switchOrder()
+    // {
+    //     // création d'une nouvelle liste chainée
+    //     let inversedRathPath = new ListeChainee();
+
+    //     // on regarde le premier maillon (la tête)
+    //     let current = this._head;
+
+    //     while (current != null) {
+    //         // ajout du nouveau maillon
+    //         inversedRathPath.ajouter(current.id, this._tail.x, this._tail.y, current.velX * (-1), current.velY * (-1), current.switchAnim());
+
+    //         // on regarde le maillon d'après
+    //         current = current.next;
+    //     }
+
+    //     return inversedRathPath;
+    // }
 }
 
 
@@ -169,3 +285,6 @@ ratPath.ajouter(3, 700, 400, 200, -200, "up-rat");
 ratPath.ajouter(2, 600, 500, 200, 0, "right-rat");
 ratPath.ajouter(1, 250, 500, 200, 200, "right-rat");
 ratPath.ajouter(0, 150, 400, 0, 200, "down-rat");
+
+ratPath.decalage();
+ratPath.affichageGraph();

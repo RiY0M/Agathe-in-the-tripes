d436398b7020b7420231af9092b54192347efa41
SCHANDEL Victor <victor.schandel8@etu.univ-lorraine.fr>
2024-01-11 15:17:58 +0000

    Update 3 files
    - /depricated/api_leaderboard.js
    - /depricated/createObstaclesDeprecated.js
    - /depricated/database.js

--- depricated/database.js
+++
@@ -1,7 +0,0 @@
-export const dbConfig = {
-    host: 'devbdd.iutmetz.univ-lorraine.fr',
-    user: 'ponzo1u_appli',
-    password: '72622503',
-    database: 'ponzo1u_sae501',
-    multipleStatements: true //! Pas sécurisé
-}

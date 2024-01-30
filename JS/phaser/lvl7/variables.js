2179b4b569f8b2d84a72c5a2764dca15e40c9c7c
Yoni Rigaut <yoni.rigaut6@etu.univ-lorraine.fr>
2024-01-30 09:52:48 +0100

    changement des variables changemiv

--- JS/phaser/lvl7/variables.js
+++ JS/phaser/lvl7/variables.js
@@ -3,8 +3,8 @@ let cursors;                // détection clavier
 let lastFrame = 8;          // last frame facing afk
 let music;                  //Theme (musique de fond)
 
-const idCurrentLvl = 4;      // Id du niveau courant
-const idNextLvl = 5;        // Id du prochain niveau
+const idCurrentLvl = 7;      // Id du niveau courant
+const idNextLvl = 8;        // Id du prochain niveau
 let startTime = new Date().getTime(); // Starting time for the chronometer
 
 let start3sCoolDownAgathe = false;    // lancement de l'invincibilité pour Agathe

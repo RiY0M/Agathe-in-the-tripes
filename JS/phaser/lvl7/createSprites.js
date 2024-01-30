1081ae8fb201aaa888db25605e20c1ba79304485
V. Schandel <schandel2u>
2024-01-30 14:59:17 +0100

    PV modif

--- JS/phaser/lvl7/createSprites.js
+++ JS/phaser/lvl7/createSprites.js
@@ -16,16 +16,16 @@ function createMap(scene){
 //* Créer et gère les PV de barre de vie *//
 function setHealthBar(value){   //Fonction qui s'occupe de la barre de vie du boss
     width_bar = 700; //Taille bar de vie
-    percent_bar = Phaser.Math.Clamp(value, 0, 20) / 20; //Nb de pv : Ici de 0 -> 100
+    percent_bar = Phaser.Math.Clamp(value, 0, 21) / 21; //Nb de pv : Ici de 0 -> 21
 
     const centerX = (config.width - width_bar) / 2;
 
     graphics.clear();
     graphics.fillStyle(0xff6600);
-    graphics.fillRoundedRect(centerX, 550, width_bar, 20, 5);
+    graphics.fillRoundedRect(centerX, 550, width_bar, 21, 5);
 
     if (percent_bar > 0){
         graphics.fillStyle(0xff0000);
-        graphics.fillRoundedRect(centerX, 550, width_bar * percent_bar, 20, 5)
+        graphics.fillRoundedRect(centerX, 550, width_bar * percent_bar, 21, 5)
     }
 }
--- JS/phaser/lvl7/variables.js
+++ JS/phaser/lvl7/variables.js
@@ -27,5 +27,5 @@ let graphics;
 let boss;
 let percent_bar;
 let width_bar
-let bossHealth = 20; // PV initiale du boss
+let bossHealth = 21; // PV initiale du boss
 let isInvicibleBoss = false;        // Bool : mob invicible ou non
